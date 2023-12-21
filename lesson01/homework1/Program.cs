cb6498d8d7760285a1c3a7c65a430f2b7fa85a2e
belyankiss <belyankiss@gmail.com>
2023-12-21 11:20:35 +0300

    Created a new dirs and files with homework and if/else

---
+++ lesson01/homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Doing homework
+
+int a = 3;
+int b = 8;
+int c = 4;
+int d = 5;
+
+double multi = a * b;
+double sum = c + d;
+
+int equation = (a * b) / (c + d);
+double equation2 = multi / sum;
+Console.WriteLine(equation);
+Console.WriteLine(equation2);
---
+++ lesson01/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 7000;
+int b = 100;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
